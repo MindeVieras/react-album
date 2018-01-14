1e5c4f96c1af8509151b9cd6ae5084a3ed334404
MindeVieras <wieraz21@gmail.com>
2018-01-14 21:50:52 +0000

    slick removed css media scroll instead

--- dev/js/_components/Front/Album/Media.jsx
+++ dev/js/_components/Front/Album/Media.jsx
@@ -1,31 +1,6 @@
 
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import Slider from 'react-slick'
-
-import { FaChevronLeft, FaChevronRight } from 'react-icons/lib/fa'
-
-function NextArrow(props) {
-  const {className, style, onClick} = props
-  return (
-    <div
-      className={`${className} arrow next`}
-      style={{...style}}
-      onClick={onClick}
-    ><FaChevronRight /></div>
-  )
-}
-
-function PrevArrow(props) {
-  const {className, style, onClick} = props
-  return (
-    <div
-      className={`${className} arrow prev`}
-      style={{...style}}
-      onClick={onClick}
-    ><FaChevronLeft /></div>
-  )
-}
 
 class Media extends Component {
   
@@ -36,26 +11,15 @@ class Media extends Component {
   
   render(){
     const { media } = this.props
-    const settings = {
-      dots: false,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      variableWidth: true,
-      nextArrow: <NextArrow />,
-      prevArrow: <PrevArrow />
-    }
     return (
-
-      <div className="media">
-        <Slider {...settings}>
-          {media.map((media, i) => 
-            <div className="media-item" key={ i }>
-              <img src={ media.key } />
-            </div>
-          )}
-        </Slider>
+      <div
+        className="media"
+      >
+        {media.map((media, i) => 
+          <div className="media-item" key={ i }>
+            <img src={ media.key } />
+          </div>
+        )}
       </div>
     )
   }
