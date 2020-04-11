e2f6b7103b93b20f0ec1b3d660b2ba8eb5daa7a7
MindeVieras <mindevieras@gmail.com>
2020-04-11 01:05:48 +0100

    aws bucket and accessKey is added to config

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -6,4 +6,8 @@ export const config = {
   isDev: process.env.NODE_ENV === 'development',
   baseServerUrl: process.env.REACT_APP_BASE_URL!,
   recaptchaSiteKey: process.env.REACT_APP_RECAPTCHA_SITE_KEY!,
+  aws: {
+    bucket: process.env.REACT_APP_AWS_BUCKET!,
+    accessKey: process.env.REACT_APP_AWS_ACCESS_KEY!,
+  },
 }
